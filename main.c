7e8eb9e7c2c3f2e1dba007bfc07d1889e7fcf6ab
jonastadeu <jonasreichert@gmail.com>
2016-08-26 22:19:01 -0300

    atributos adicionados

--- main.c
+++ main.c
@@ -18,14 +18,41 @@ int main(int argc, char *argv[])
 		// laco que varre a coluna
 		for (k = 0; k < 4; k++)
 		{
-			// insere um valor randomico em uma posicao da matriz
-			matriz[j][k] = rand()%50;
+			// insere o indice da coluna na matriz
+			matriz[j][k] = k;
 			// imprime o valor da posicao atual da matriz
 			printf("%02d ", matriz[j][k]);
 		}
 		// quebra a linha
 		printf("\n");
 	}
+	
+	// imprime uma mensagem de identificacao
+	printf("\nOs elementos da diagonal principal sao:\n");
+	// efetua a varredura da matriz
+	for(j = 0; j < 4; j++)
+	{	
+		// imprime os elementos da diagonal
+		printf("%02d ", matriz[j][j]);
+	}
+	
+	// imprime uma mensagem de identificacao
+	printf("\n\nOs outros elementos sao:\n");
+	// laco que varre a linha
+	for(j = 0; j < 4; j++)
+	{
+		// laco que varre a coluna
+		for(k = 0; k < 4; k++)
+		{
+			// se os indices forem iguais (diagonal principal)
+			if (j==k) printf("   ");
+			// senao imprime o valor da posicao atual da matriz
+			else printf("%02d ", matriz[j][k]);
+		}
+		// quebra a linha
+		printf("\n");
+	}
+	
 	// retorna 0
 	return 0;
 }
